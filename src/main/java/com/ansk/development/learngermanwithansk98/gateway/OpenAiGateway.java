bfbd6493aef407aaf8ac8d1ef06aaf9e7ab84a45
Anton Skripin <ansk98@ansk98>
2024-08-30 20:11:50 +0200

    Prompts for ai

---
+++ src/main/java/com/ansk/development/learngermanwithansk98/gateway/OpenAiGateway.java
@@ -0,0 +1,12 @@
+package com.ansk.development.learngermanwithansk98.gateway;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Gateway to communicate with the OpenAI.
+ *
+ * @author Anton Skripin
+ */
+@Component
+public class OpenAiGateway {
+}
