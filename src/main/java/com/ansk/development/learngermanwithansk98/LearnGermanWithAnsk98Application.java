48e57223d6b59dd2ccb968b7998feb023a6a12ca
Anton Skripin <ansk98@ansk98>
2024-09-03 22:07:03 +0200

    Restart bot if gets offline

--- src/main/java/com/ansk/development/learngermanwithansk98/LearnGermanWithAnsk98Application.java
+++ src/main/java/com/ansk/development/learngermanwithansk98/LearnGermanWithAnsk98Application.java
@@ -3,7 +3,9 @@ package com.ansk.development.learngermanwithansk98;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class LearnGermanWithAnsk98Application {
--- src/main/java/com/ansk/development/learngermanwithansk98/gateway/DailyDeutschBotConsumer.java
+++ src/main/java/com/ansk/development/learngermanwithansk98/gateway/DailyDeutschBotConsumer.java
@@ -3,10 +3,14 @@ package com.ansk.development.learngermanwithansk98.gateway;
 import com.ansk.development.learngermanwithansk98.config.DailyDeutschBotConfiguration;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * A consumer class for handling updates in a single-threaded, long-polling manner for the Daily Deutsch Bot.
@@ -20,6 +24,7 @@ public class DailyDeutschBotConsumer implements LongPollingSingleThreadUpdateCon
     private final DailyDeutschBotConfiguration config;
     private final FilterChain filterChain;
     private final InputGateway inputGateway;
+    private TelegramBotsLongPollingApplication longPollingApplication;
 
 
     public DailyDeutschBotConsumer(DailyDeutschBotConfiguration config,
@@ -32,18 +37,35 @@ public class DailyDeutschBotConsumer implements LongPollingSingleThreadUpdateCon
 
     @EventListener(ApplicationStartedEvent.class)
     public void registerBot() {
-        try {
-            TelegramBotsLongPollingApplication longPollingApplication = new TelegramBotsLongPollingApplication();
-            longPollingApplication.registerBot(config.token(), this);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+        longPollingApplication = new TelegramBotsLongPollingApplication();
+        registerBotInternal();
     }
 
     @Override
     public void consume(Update update) {
         filterChain.filter(update);
         inputGateway.process(update);
+    }
+
+    @Scheduled(initialDelay = 10, fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
+    public void ensureBotRunning() {
+        if (longPollingApplication.isRunning()) {
+            return;
+        }
+
+        try {
+            longPollingApplication.start();
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void registerBotInternal() {
+        try {
+            longPollingApplication.registerBot(config.token(), this);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
