252a05d10c1dbc712cb7a2483f6c5aeb2673857b
ansk98 <antonskripin@gmail.com>
2024-09-11 22:53:20 +0200

    Pom and test config

--- src/test/java/com/ansk/development/learngermanwithansk98/DailyDeutschBotTestConfig.java
+++ src/test/java/com/ansk/development/learngermanwithansk98/DailyDeutschBotTestConfig.java
@@ -2,25 +2,28 @@ package com.ansk.development.learngermanwithansk98;
 
 import com.ansk.development.learngermanwithansk98.gateway.telegram.DailyDeutschBotConsumer;
 import com.ansk.development.learngermanwithansk98.config.DailyDeutschBotConfiguration;
+import com.ansk.development.learngermanwithansk98.gateway.telegram.ITelegramOutputGateway;
+import com.ansk.development.learngermanwithansk98.gateway.telegram.TelegramOutputGateway;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @TestConfiguration
 public class DailyDeutschBotTestConfig {
 
     @Bean
     @Primary
-    public DailyDeutschBotConsumer dailyDeutschBot() {
+    public DailyDeutschBotConsumer dailyDeutschBotConsumer() {
         return Mockito.mock(DailyDeutschBotConsumer.class);
     }
 
     @Bean
     @Primary
-    public DailyDeutschBotConfiguration dailyDeutschBotConfig() {
-        DailyDeutschBotConfiguration mockConfig = Mockito.mock(DailyDeutschBotConfiguration.class);
-        Mockito.when(mockConfig.getToken()).thenReturn("12345:YOUR_TOKEN");
-        return mockConfig;
+    public TelegramOutputGateway telegramOutputGateway() {
+        return Mockito.mock(TelegramOutputGateway.class);
     }
 }
