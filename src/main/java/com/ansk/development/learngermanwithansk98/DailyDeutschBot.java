eeea415991daccd7ad648a72574a50d88a8291cf
ansk98 <antonskripin@gmail.com>
2024-09-21 23:38:33 +0200

    Test

--- src/main/java/com/ansk/development/learngermanwithansk98/DailyDeutschBot.java
+++ src/main/java/com/ansk/development/learngermanwithansk98/DailyDeutschBot.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DailyDeutschBot {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         SpringApplication.run(DailyDeutschBot.class, args);
     }
 
