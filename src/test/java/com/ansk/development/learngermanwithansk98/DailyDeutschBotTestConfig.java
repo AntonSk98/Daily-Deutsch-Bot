5dbb961e59a45044e51cd2d73771727f0f84dff5
ansk98 <anton.tech98@gmail.com>
2024-10-05 14:19:29 +0200

    Fixed test parts and removed unused

--- src/test/java/com/ansk/development/learngermanwithansk98/DailyDeutschBotTestConfig.java
+++ src/test/java/com/ansk/development/learngermanwithansk98/DailyDeutschBotTestConfig.java
@@ -1,24 +1,26 @@
 package com.ansk.development.learngermanwithansk98;
 
 import com.ansk.development.learngermanwithansk98.integration.telegram.DailyDeutschBotConsumer;
+import com.ansk.development.learngermanwithansk98.integration.telegram.ITelegramClient;
 import com.ansk.development.learngermanwithansk98.integration.telegram.TelegramClient;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 
 @TestConfiguration
 public class DailyDeutschBotTestConfig {
 
     @Bean
     @Primary
-    public DailyDeutschBotConsumer dailyDeutschBotConsumer() {
+    public LongPollingSingleThreadUpdateConsumer dailyDeutschBotConsumer() {
         return Mockito.mock(DailyDeutschBotConsumer.class);
     }
 
     @Bean
     @Primary
-    public TelegramClient telegramOutputGateway() {
+    public ITelegramClient telegramClient() {
         return Mockito.mock(TelegramClient.class);
     }
 }
